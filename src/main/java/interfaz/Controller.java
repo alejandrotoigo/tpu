3afa4d9fc05d0413eb54266f471610d96da9288b
MaxiAlbert <Maximilianoalbert96@gmail.com>
2020-11-08 18:25:19 -0300

    Faltaba eso

--- src/main/java/interfaz/Controller.java
+++ src/main/java/interfaz/Controller.java
@@ -1,4 +1,101 @@
 package interfaz;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.Cursor;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import negocio.Agrupaciones;
+import negocio.Region;
+import negocio.Regiones;
+import negocio.Resultados;
+
+import java.io.File;
+
 public class Controller {
+    public Label lblUbicacion;
+    public ListView lvwResultados;
+    public ComboBox cboDistritos;
+    public ComboBox cboSecciones;
+    public ComboBox cboCircuitos;
+    public Resultados resultados;
+    public Label lblCargaDatosInfo;
+
+    public void cambiarUbicacion(ActionEvent actionEvent) {
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setTitle("Seleccione ubicación de los datos");
+        dc.setInitialDirectory(new File("C:\\Users"));
+        //dc.setInitialDirectory(new File(System.getProperty("user.dir")));
+        File file = dc.showDialog(null);
+        if (file != null)
+            lblUbicacion.setText(file.getPath());
+    }
+
+// *********hasta acá controlado*********
+
+    public void cargarDatos(ActionEvent actionEvent) {
+        ObservableList ol;
+        //Generamos lista de agrupaciones
+        lblCargaDatosInfo.setText("Cargando Datos al sistema. Por favor espere.");
+        Agrupaciones.leerAgrupaciones(lblUbicacion.getText());
+//HASTA ACA BIEN - PROBLEMAS EN LA CARGA DE DISTRITOS Y POR ENDE EN LA CARGA DE REGIONES, PERO NO DE CIRCUITOS
+        //Generamos lista de distritos del país
+        Regiones regiones = new Regiones(lblUbicacion.getText());
+        ol = FXCollections.observableArrayList(regiones.getDistritos());
+        cboDistritos.setItems(ol);
+        //Procesamos los totales por región
+        resultados = new Resultados(lblUbicacion.getText());
+        //System.out.println(resultados);
+        ol = FXCollections.observableArrayList(resultados.getResultadosRegion("00"));
+        lvwResultados.setItems(ol);
+        lblCargaDatosInfo.setText("Datos Cargados en el sistema.");
+
+    }
+
+    public void elegirDistrito(ActionEvent actionEvent) {
+        //Generamos lista de secciones del distrito elegido
+        ObservableList ol;
+
+        Region distrito = (Region) cboDistritos.getValue();
+        ol = FXCollections.observableArrayList(distrito.getSubregiones());
+        cboSecciones.setItems(ol);
+        //Mostramos resultados del Distrito
+        ol = FXCollections.observableArrayList(resultados.getResultadosRegion(distrito.getCodigo()));
+        lvwResultados.setItems(ol);
+
+    }
+
+    // ***********controlado desde acá hasta el final***********
+    public void elegirSeccion(ActionEvent actionEvent) {
+        ObservableList ol;
+        //Genera una lista de circuitos de la sección elegida
+        if (cboSecciones.getValue() != null) {
+            Region seccion = (Region) cboSecciones.getValue();
+            ol = FXCollections.observableArrayList(seccion.getSubregiones());
+            cboCircuitos.setItems(ol);
+            //Mostramos resultados de la sección
+            ol = FXCollections.observableArrayList(resultados.getResultadosRegion(seccion.getCodigo()));
+            lvwResultados.setItems(ol);
+        } else {
+            cboCircuitos.setItems(null);
+        }
+    }
+
+    public void elegirCircuito(ActionEvent actionEvent) {
+        ObservableList ol;
+        //Genera una lista de circuitos de la sección elegida
+        if (cboCircuitos.getValue() != null) {
+            Region circuito = (Region) cboCircuitos.getValue();
+            //Mostramos resultados del circuito
+            ol = FXCollections.observableArrayList(resultados.getResultadosRegion(circuito.getCodigo()));
+            lvwResultados.setItems(ol);
+        } else {
+            cboCircuitos.setItems(null);
+        }
+    }
 }
--- src/main/java/util/TextFile.java
+++ src/main/java/util/TextFile.java
@@ -1,65 +1,146 @@
 package util;
 
-import modelos.TSBHashtableDA;
-
 import negocio.Agrupacion;
-
+import negocio.Region;
+import negocio.Resultados;
+import modelos.TSBHashtableDA;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TextFile {
-    private File archivo;
-
+    private File file;
 
     public TextFile(String path) {
-        this.archivo = new File(path);
-
-
-
+        file = new File(path);
     }
 
     public String leerEncabezado() {
-
         String linea = "";
         try {
-            Scanner scanner = new Scanner(archivo);
+            Scanner scanner = new Scanner(file);
             while (scanner.hasNext()) {
                 linea = scanner.nextLine();
                 break;
             }
-
         } catch (FileNotFoundException e) {
-            System.out.println("No se pudo leer el Archivo");
+            System.out.println("No se pudo leer el archivo");
         }
         return linea;
-
-
     }
 
-    public TSBHashtableDA identificarAgrupacione() {
-        String linea = "";
+    public TSBHashtableDA identificarAgrupaciones() {
+        String linea = "", campos[];
         TSBHashtableDA table = new TSBHashtableDA(10);
-
+        Agrupacion agrupacion;
         try {
-            Scanner scanner = new Scanner(archivo);
+            Scanner scanner = new Scanner(file);
             while (scanner.hasNext()) {
                 linea = scanner.nextLine();
-                String[] campos = linea.split("\\|");
-                if (campos[0].compareTo("000100000000000") == 0){
-                    Agrupacion agrupacion = new Agrupacion(campos[2],campos[3]);
-                    table.put(agrupacion.getCodigo(),agrupacion);
-
+                campos = linea.split("\\|");
+                //Filtramos votación para Presidente
+                if (campos[0].compareTo("000100000000000") == 0) {
+                    agrupacion = new Agrupacion((campos[2]), campos[3]);
+                    table.put(agrupacion.getCodigo(), agrupacion);
                 }
-
-
-                
             }
-
         } catch (FileNotFoundException e) {
-            System.out.println("No se pudo leer el Archivo");
+            System.out.println("No se pudo leer el archivo");
         }
         return table;
-
     }
+    // METODO DE ABAJO CONTROLADO
+    public void sumarVotosPorRegion(Resultados resultados) {
+        String linea = "", campos[], codAgrupacion;
+
+        int votos;
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                linea = scanner.nextLine();
+                campos = linea.split("\\|");
+                codAgrupacion = campos[5];
+                //Filtramos votación para Presidente
+                    if (campos[4].compareTo("000100000000000") == 0) {
+                    votos = Integer.parseInt(campos[6]);
+                    //Acumulamos los votos del pais
+                    resultados.sumarVotos("00",codAgrupacion,votos);
+                    //Acumulamos los votos del distrito, seccion y circuito de la mesa
+                    //resultados.sumarVotos(campos[0],campos[5],votos);
+                    for(int i = 0; i<3; i++){
+                        resultados.sumarVotos(campos[i],codAgrupacion,votos);
+
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se pudo leer el archivo");
+        }
+    }
+
+    public void sumarVotosPorAgrupacion(TSBHashtableDA table) {
+        String linea = "", campos[];
+        Agrupacion agrupacion;
+        int votos;
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                linea = scanner.nextLine();
+                campos = linea.split("\\|");
+                //Filtramos votación para Presidente
+                if (campos[4].compareTo("000100000000000") == 0) {
+                    agrupacion = (Agrupacion) table.get(campos[5]);
+                    votos = Integer.parseInt(campos[6]);
+                    agrupacion.sumarVotos(votos);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se pudo leer el archivo");
+        }
+    }
+    //METODO DE ABAJO CONTROLADO
+    public Region identificarRegiones() {
+        String linea = "", campos[], codigo, nombre;
+        Region pais = new Region("00", "Argentina");
+        Region distrito, seccion;
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                linea = scanner.nextLine();
+                campos = linea.split("\\|");
+
+                codigo = campos[0];
+    //CONTROLADO HASTA ACA
+
+                nombre = campos[1];
+                switch(codigo.length()){
+                    case 2:
+                        //Distrito
+                            distrito = pais.getOrPutSubregion(codigo);
+                            distrito.setNombre(nombre);
+                            break;
+                    case 5:
+                        //Seccion
+                            distrito = pais.getOrPutSubregion(codigo.substring(0,2));
+                            seccion = distrito.getOrPutSubregion(codigo);
+                            seccion.setNombre(nombre);
+                            break;
+                    case 11:
+                        //Circuito
+                        distrito = pais.getOrPutSubregion(codigo.substring(0,2));
+                        seccion = distrito.getOrPutSubregion(codigo.substring(0,5));
+                        seccion.agregarSubregion(new Region(codigo,nombre));
+
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se pudo leer el archivo");
+        }
+        return pais;
+    }
+
+
 }
+
